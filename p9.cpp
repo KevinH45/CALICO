689f8fa2493454638360b06fadd8423b070a0ba1
tkl0000 <thomasli2025@gmail.com>
2023-11-18 22:49:59 -0500

    p9

--- p9.cpp
+++ p9.cpp
@@ -14,6 +14,21 @@
 
 using namespace std;
 
+set<int> sink;
+set<int> source;
+int num_paths;
+int *connections;
+
+int dfs(int i, vector<int> *adj) {
+    if (sink.find(i) != sink.end()) {
+        num_paths += 1;
+        return 1;
+    }
+    for (int a : adj[i]) {
+        connections[i] += dfs(a, adj);
+    }
+}
+
 int main() {    
     int T; cin >> T;
     for (int t = 0; t < T; ++t) {
@@ -21,8 +36,6 @@ int main() {
         int M; cin >> M;
         int S; cin >> S;
         vector<int> adj[N];
-        set<int> sink;
-        set<int> source;
         for (int x = 0; x < N; ++x) source.insert(x);
         for (int x = 0; x < N; ++x) adj[x] = vector<int>();
         for (int x = 0; x < M; ++x) {
@@ -32,11 +45,21 @@ int main() {
         }
         for (int x = 0; x < N; ++x) {
             if (adj[x].size() == 0) sink.insert(x);
-            for (int i : adj[x]) source.erase(x);
+            for (int i : adj[x]) source.erase(i);
         }
-        int connections[N];
+        connections = new int[N];
+        num_paths = 0;
         for (int x = 0; x < N; ++x) connections[x] = 0;
-        
-
+        for (int i : source) {
+            dfs(i, adj);
+        }
+        for (int x = 0; x < N; ++x) {
+            if (source.find(x) == source.end() && connections[x] == num_paths) {
+                cout << x + 1 << endl;
+                return 0;
+            }
+        }
+        cout << "IMPOSSIBLE" << endl;
+        return 0;
     }
 }
