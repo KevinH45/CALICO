0945016f7bc7688bc8da66226058493a0823f6d9
tkl0000 <thomasli2025@gmail.com>
2023-11-18 21:39:00 -0500

    new

--- p6.cpp
+++ p6.cpp
@@ -18,5 +18,17 @@ int main() {
     for (int t = 0; t < T; ++t) {
         int N; cin >> N;
         int K; cin >> K;
+        vector<int> vec;
+        for (int x = 1; x <= N; ++x) vec.push_back(x);
+        for (int x = 0; x < N; ++x) {
+            vec.push_back(vec[x]);
+            vec.erase(vec.begin() + x);
+        }
+        for (int x = 0; x < N; ++x) {
+            if (vec[x] == K) {
+                cout << x+1 << endl;
+                break;
+            }
+        }
     }
 }
